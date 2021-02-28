d9a37f88dc284095fb6bc567a0b58ed6ed4d2507
MrBanh <tonybanh@live.com>
2021-02-27 19:38:15 -0800

    Refactor: move add category form to separate php file

---
+++ view/add_category_form.php
@@ -0,0 +1,7 @@
+<h2>Add Category</h2>
+<form action="." method="POST">
+    <input type="hidden" name="action" value="add_category">
+    <label for="categoryName">Name: </label>
+    <input type="text" name="categoryName" id="categoryName">
+    <button class="add_btn">Add Category</button>
+</form>
--- view/category_list.php
+++ view/category_list.php
@@ -31,13 +31,7 @@
         <?php } ?>
     </div>
 
-    <h2>Add Category</h2>
-    <form action="." method="POST">
-        <input type="hidden" name="action" value="add_category">
-        <label for="categoryName">Name: </label>
-        <input type="text" name="categoryName" id="categoryName">
-        <button class="add_btn">Add Category</button>
-    </form>
+    <?php include ('view/add_category_form.php'); ?>
 
     <p><a href=".">View To Do List</a></p>
 </section>
