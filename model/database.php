2ffb0103f597c76f7ecf33f3adac5e554a3a3173
Tony <25971310+MrBanh@users.noreply.github.com>
2021-03-06 03:25:47 -0800

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,11 +1,11 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=todolist';
-    $username = 'root';
-    // $password = 'sesame';
+    $dsn = 'mysql:host=lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=po05qwrstus3lqef';
+    $username = 'rahry4259ykbayrv';
+    $password = 'xnytpzul5bviio06';
 
     try {
-        $db = new PDO($dsn, $username);      // without password
-        // $db = new PDO($dsn, $username, $password);
+//         $db = new PDO($dsn, $username);      // without password
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = 'Database Error: ';
         $error_message .= $e->getMessage();
